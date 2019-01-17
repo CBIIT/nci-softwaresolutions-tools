6bda7b27e963d9ae0e11c4938b774c2dd7021c45
Brian Park <park-brian@users.noreply.github.com>
2019-01-17 16:52:19 -0500

    simplify server

--- express-angular-spa/server/server.js
+++ express-angular-spa/server/server.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
-const config = require('../config.json');
-const router = require('./routes/router');
+const helmet = require('helmet');
 const logger = require('./logger');
-
+const config = require('../config.json');
 const app = express();
-const port = config.port || 3000;
 
-// use config variables in our templates
-Object.assign(app.locals, config);
-
-// use morgan to log requests during development
-if (!config.production)
-    app.use(morgan('[:date[iso]] [:method] :url :status'));
-
-// register routes and start application
-app.use(router);
-app.listen(port, e => {
-    logger.info(`Application is running on port ${port}`);
-});
+app.use(helmet());
+app.use(require('./routes'));
+app.listen(config.port, () =>
+    logger.info(`Application started on port ${config.port}.`)
+);
