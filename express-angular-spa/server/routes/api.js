66b8919c318579473c2e187e6dc68fcaac106b5b
Brian Park <park-brian@users.noreply.github.com>
2019-01-17 17:25:45 -0500

    add cors to api

--- express-angular-spa/server/routes/api.js
+++ express-angular-spa/server/routes/api.js
@@ -1,6 +1,8 @@
+const cors = require('cors');
 const express = require('express');
 const router = express.Router();
 
+router.use(cors());
 router.get('/ping', (req, res) => res.json(true));
 
 module.exports = router;
